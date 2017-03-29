99c4715bf566d5111464ef21b9c7c9ef5e9f3249
kfwong <kfwong@zoho.com>
2017-03-29 20:51:04 +0800

    Added a form to register repo.

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,12 @@ router.get('/', function(req, res, next) {
 	res.send('API base route. Nothing here.');
 });
 
+router.get('/repos/register', function(req, res, next) {
+	exec('find ./git -mindepth 2 -maxdepth 2', function(err, stdout, stderr){
+		res.render('repo', {dir: stdout});
+	});
+});
+
 router.post('/repos/register', function(req, res, next) {
 	var repoUrl = req.body.repoUrl;
 	var repoUrlParams = repoUrl.split('/');
@@ -22,9 +28,9 @@ router.post('/repos/register', function(req, res, next) {
 
 	git().clone('https://github.com/'+repoOwner+'/'+repoName, 'git/'+repoOwner+'/'+repoName, [], function(err, data){
 		if(!err){
-			res.send('repo cloned.');
+			res.send('repo cloned, endpoint for '+req.headers.host+'/api/repos/'+repoOwner+'/'+repoName+' is now available; try <a href="'+req.protocol+'://'+req.headers.host+'/api/repos/'+repoOwner+'/'+repoName+'/?command=log --oneline">'+req.protocol+'://'+req.headers.host+'/api/repose/'+repoOwner+'/'+repoName+'/?command=log --oneline'+'</a>');
 		} else if(err.indexOf('already exists') > 0){
-			res.send('repo already cloned, skipped.');
+			res.send('repo already cloned, skipped; endpoint for '+req.headers.host+'/api/repos/'+repoOwner+'/'+repoName+' is available; try <a href="'+req.protocol+'://'+req.headers.host+'/api/repos/'+repoOwner+'/'+repoName+'/?command=log --oneline">'+req.protocol+'://'+req.headers.host+'/api/repose/'+repoOwner+'/'+repoName+'/?command=log --oneline'+'</a>');
 		}else{
 			res.send('unknown error.');
 		}
